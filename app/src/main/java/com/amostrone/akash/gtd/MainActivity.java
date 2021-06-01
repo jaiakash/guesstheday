1d6053f4b75a1f98e4ab8c09082f29cf49b76181
Akash Jaiswal <akashjaiswal3846@gmail.com.com>
2021-06-01 22:57:27 +0530

    Layout Added 4 option buttons Added question text

--- app/src/main/java/com/amostrone/akash/gtd/MainActivity.java
+++ app/src/main/java/com/amostrone/akash/gtd/MainActivity.java
@@ -3,12 +3,16 @@ package com.amostrone.akash.gtd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView date_text = (TextView) findViewById(R.id.text_date);
+        date_text.setText("Akash");
     }
 }
